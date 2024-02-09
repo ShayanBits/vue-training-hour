f5de213de5b1d2380fa712192cfe4397eb8e6c04
Shayan <sshahpasand@wescale.com>
2024-02-09 14:59:42 +0100

    ✨ Introduce named slots with combination of default slots

---
+++ src/components/MainComponentNormalSlot.vue
@@ -0,0 +1,33 @@
+<template>
+    
+    <Cart>
+        <template #firstName>
+            Mr X
+        </template>
+            Mr X family name
+        <template v-slot:id>
+            XXX
+        </template>
+    </Cart>
+
+    <Cart v-for="item in myItems">
+        {{ item.name }}
+        {{ item.lastName }}
+        <template #id>
+            {{ item.id }}
+        </template>
+    </Cart>
+
+</template>
+
+<script setup>
+import Cart from './Cart.vue';
+
+const myItems= [
+    {name:"John", lastName: "Wick", id: "XXX"},
+    {name:"John", lastName: "Doe", id: "YYY "},
+    {name:"Albert", lastName: "Ion", id: "XXX "},
+    {name:"Walter", lastName: "White", id: "XXX "},
+]
+
+</script>
